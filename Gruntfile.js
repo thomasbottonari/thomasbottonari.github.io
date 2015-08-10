624e22d741f189d036c09bbbd199281580f0606d
thomasbottonari <thomas.bottonari@gmail.com>
2015-08-10 13:04:25 -0400

    setup grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,117 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      build: [
+        'build'
+      ]
+    },
+    copy: {
+      prod: {
+        files: [
+          {expand: true, src: ['fonts/*'], dest: 'build/', filter: 'isFile'},
+          {expand: true, src: ['libs/**'], dest: 'build/'},
+          {expand: true, src: ['images/**'], dest: 'build/'},
+          {expand: true, src: ['*.html'], dest: 'build/', filter: 'isFile'},
+        ],
+      },
+      csslibs: {
+        files: [
+          {expand: true, src: ['css/lib/*.css'], dest: 'build/', filter: 'isFile'}
+        ]
+      }
+    },
+    jshint: {
+      prod: [
+        '/pages/*.js',
+        'Gruntfile.js' ]
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      prod: {
+        files: {
+//          'build/js/app/gallery.js': 'js/app/gallery.js'
+        }
+      }
+    },
+    less: {
+      dev: {
+        options: {
+          paths: ["css"]
+        },
+        files: {
+          "css/index.css": "css/index.less"
+        }
+      },
+      prod: {
+        options: {
+          paths: ["css"],
+          // plugins: [
+          //   new (require('less-plugin-autoprefix'))({browsers: ["last 2 versions"]}),
+          //   new (require('less-plugin-clean-css'))(cleanCssOptions)
+          // ]
+        },
+        files: {
+            "build/css/index.css": "css/index.less",
+        }
+      }
+    },
+    cssmin: {
+      prod: {
+        expand: true,
+        cwd: 'build/css',
+        src: ['*.css'],
+        dest: 'build/css'
+      }
+    },
+//    responsive_images: {
+//      options: {
+//        newFilesOnly: true,
+//        sizes: [{
+//          name: "medium",
+//          width: 1024
+//        }, {
+//          name: "thumb",
+//          width: 190
+//        }],
+//        rename: false
+//      },
+//      files: {
+//        expand: true,
+//        src: ['**.{JPG,jpg,PNG,png}'],
+//        cwd: 'images/gallery/',
+//        custom_dest: 'images/gallery/{%= name %}/'
+//      }
+//    }
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+//  grunt.loadNpmTasks('grunt-responsive-images');
+
+  // Development task(s).
+  grunt.registerTask('debug', [
+    'jshint:prod',
+    'less:dev'
+  ]);
+  // Production task(s).
+  grunt.registerTask('release', [
+    'clean:build',
+//    'responsive_images',
+    'copy:prod',
+    'jshint:prod', 
+    'uglify:prod', 
+    'less:prod', 
+    'cssmin:prod',
+    'copy:csslibs'
+  ]);
+};
